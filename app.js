b4132012b7542f4d931f1023a0427fa28dd2630a
Pete8219 <psavrasov@gmail.com>
2020-05-16 22:40:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+const router = require("./router")
+
+app.listen(3000)
+
+app.use(express.static("public"))
+
+app.set("views", "views")
+app.set("view engine", "ejs")
+
+app.use("/", router)
